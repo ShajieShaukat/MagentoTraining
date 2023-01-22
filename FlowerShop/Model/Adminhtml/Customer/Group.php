764ec0742d3a30f4edcdcda3b94c82aadf8630a9
M.Shajie Shaukat <shajie.shaukat@rltsquare.com>
2023-01-22 15:55:52 +0500

    Flower Shop added

---
+++ FlowerShop/Model/Adminhtml/Customer/Group.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace RLTSquare\FlowerShop\Model\Adminhtml\Customer;
+
+use Magento\Customer\Model\ResourceModel\Group\CollectionFactory;
+use Magento\Framework\Option\ArrayInterface;
+
+/**
+ * Class for get customer groups
+ */
+class Group implements ArrayInterface
+{
+
+    protected $_options;
+    /**
+     * @var CollectionFactory
+     */
+    private CollectionFactory $_groupCollectionFactory;
+
+    /**
+     * @param CollectionFactory $groupCollectionFactory
+     */
+    public function __construct(CollectionFactory $groupCollectionFactory)
+    {
+        $this->_groupCollectionFactory = $groupCollectionFactory;
+    }
+
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        if (!$this->_options) {
+            $this->_options = $this->_groupCollectionFactory->create()->loadData()->toOptionArray();
+        }
+        return $this->_options;
+    }
+}
---
+++ FlowerShop/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'RLTSquare_FlowerShop', __DIR__);
